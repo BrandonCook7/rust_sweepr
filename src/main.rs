1218b18b249d41cea596a33c891e4a09a89f1a04
Brandon Cook <brandoncook7000@gmail.com>
2022-09-28 15:21:30 -0700

    Graphics modifications

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,12 @@ pub struct App {
 }
 
 impl App {
-    fn render(&mut self, args: &RenderArgs, game: &GameInstance) {
+    fn render(&mut self, args: &RenderArgs, game: &mut GameInstance) {
         use graphics::*;
 
         //Get 2d vector dimensions
         let tile_size: [f64; 2] = [game.grid_size[0]/game.x_size as f64, game.grid_size[0]/game.y_size as f64];
-        //Color format is Red, Green, Blue,  Alpha
+        //Color format is Red, Green, Blue, Alpha
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         const LIGHT_RED: [f32; 4] = [1.0, 0.33, 0.2, 1.0];
@@ -70,89 +70,107 @@ impl App {
             let ts = TextureSettings::new();
             let ds = DrawState::default();
             //ts.set_border_color(GRAY);
-            let mut glyph_cache = GlyphCache::new("assets/Roboto-Medium.ttf", (), TextureSettings::new()).unwrap();
-            let mut font2 = GlyphCache::new("assets/Inversionz.otf", (), TextureSettings::new()).unwrap();
-                
+            //let mut font2 = GlyphCache::new("assets/Inversionz.otf", (), TextureSettings::new()).unwrap();
+            
+
             //Draw Menu header HERE
-            let header_background: [f64;4] = [
-                0.0,
-                game.grid_size[1],
-                game.window_size[0],
-                game.window_size[1],
-            ];
+            if game.game_state == 0{
+                let temp: [f64;4] = [
+                    0.0,
+                    50.0,
+                    50.0,
+                    50.0,
+                ];
+                //rectangle::square(30.0, 30.0, 32.0);
+                rectangle(RED, temp, c.transform.trans(game.window_size[0]/2.0, game.window_size[1]/3.0), gl);
+                let default_button_image = Image::new().rect(rectangle::square(game.window_size[0]/2.0, 80.0, 32.0));
+                let default_texture = Texture::from_path(Path::new("./assets/Images/Default_Grid.png"), &ts).unwrap();
+                default_button_image.draw(&default_texture, &DrawState::default(), c.transform, gl);
+                //game.game_state = 1;
+                //text::Text::new_color(BLACK, 12 as u32).draw(&game.flag_count.to_string(), &mut font2, &DrawState::default(), c.transform.trans(30.0, 70.0), gl).unwrap();
+            } else {
 
-            let middle_header = game.grid_size[1] + ((game.window_size[1] - game.grid_size[1])/2.0);
-            //Draws header background
-            rectangle([1.0, 1.0, 1.0, 1.0], header_background, transform, gl);
-            //Draw Flag icon
-            let flag_image = Image::new().rect(rectangle::square(0.0, middle_header-16.0, 32.0));
-            let flag_texture = Texture::from_path(Path::new("assets/tileset_01/flag.png"), &ts).unwrap();
-            flag_image.draw(&flag_texture, &ds, transform, gl);
-            //Draws flag count
-            text::Text::new_color(BLACK, 12 as u32).draw(&game.flag_count.to_string(), &mut glyph_cache, &DrawState::default(), c.transform.trans(30.0, middle_header+4.0), gl).unwrap();
-            //Draws time
-            //let mut timer = chrono::offset::Utc::now().time() - game.start_time;
-            let time_string = (self.timer as i32).to_string();
-            text::Text::new_color(BLACK, 12 as u32).draw(&time_string, &mut glyph_cache, &DrawState::default(), c.transform.trans(game.window_size[0]-20.0, middle_header+4.0), gl).unwrap();
-            //Draw clock icon
-            //TODO: Some reason clock image significantly slows down timer
-            // let clock_image = Image::new().rect(rectangle::square(game.window_size[0]-50.0, middle_header-12.0, 24.0));
-            // let clock_texture = Texture::from_path(Path::new("assets/tileset_01/clock.png"), &ts).unwrap();
-            // clock_image.draw(&clock_texture, &ds, transform, gl);
 
-            let mut x = 0.0;//x position of tile
-            let mut y = 0.0;//y position of tile
-            //let mut text = gfx_text::new(factory).build().unwrap();
-            for row in game.grid.iter() {
-                for tile in row.iter() {
-                    let rect: [f64;4] = [
-                        x * tile_size[0],
-                        y * tile_size[1],
-                        tile_size[0],
-                        tile_size[1],
-                    ];
-                    if tile.flagged {
-                        rectangle(SOFT_GRAY, rect, transform, gl);
-                        //Polygon()
-                        //polygon()
-                        //let p = c.draw_state.
-                        //rectangle()
-                    } else if tile.hidden {
-                        rectangle(WHITE, rect, transform, gl);
-                    } else {
-                        if tile.value == -1 {
-                            rectangle([1.0, 0.68, 0.68, 1.0], rect, transform, gl);
-                            
+                let header_background: [f64;4] = [
+                    0.0,
+                    game.grid_size[1],
+                    game.window_size[0],
+                    game.window_size[1],
+                ];
+                let mut glyph_cache = GlyphCache::new("assets/Roboto-Medium.ttf", (), TextureSettings::new()).unwrap();
+                let middle_header = game.grid_size[1] + ((game.window_size[1] - game.grid_size[1])/2.0);
+                //Draws header background
+                rectangle([1.0, 1.0, 1.0, 1.0], header_background, transform, gl);
+                //Draw Flag icon
+                let flag_image = Image::new().rect(rectangle::square(0.0, middle_header-16.0, 32.0));
+                let flag_texture = Texture::from_path(Path::new("assets/tileset_01/flag.png"), &ts).unwrap();
+                flag_image.draw(&flag_texture, &ds, transform, gl);
+                //Draws flag count
+                text::Text::new_color(BLACK, 12 as u32).draw(&game.flag_count.to_string(), &mut glyph_cache, &DrawState::default(), c.transform.trans(30.0, middle_header+4.0), gl).unwrap();
+                //Draws time
+                //let mut timer = chrono::offset::Utc::now().time() - game.start_time;
+                let time_string = (self.timer as i32).to_string();
+                text::Text::new_color(BLACK, 12 as u32).draw(&time_string, &mut glyph_cache, &DrawState::default(), c.transform.trans(game.window_size[0]-20.0, middle_header+4.0), gl).unwrap();
+                //Draw clock icon
+                //TODO: Some reason clock image significantly slows down timer
+                // let clock_image = Image::new().rect(rectangle::square(game.window_size[0]-50.0, middle_header-12.0, 24.0));
+                // let clock_texture = Texture::from_path(Path::new("assets/tileset_01/clock.png"), &ts).unwrap();
+                // clock_image.draw(&clock_texture, &ds, transform, gl);
+
+                let mut x = 0.0;//x position of tile
+                let mut y = 0.0;//y position of tile
+                //let mut text = gfx_text::new(factory).build().unwrap();
+                for row in game.grid.iter() {
+                    for tile in row.iter() {
+                        let rect: [f64;4] = [
+                            x * tile_size[0],
+                            y * tile_size[1],
+                            tile_size[0],
+                            tile_size[1],
+                        ];
+                        if tile.flagged {
+                            rectangle(SOFT_GRAY, rect, transform, gl);
+                            //Polygon()
+                            //polygon()
+                            //let p = c.draw_state.
+                            //rectangle()
+                        } else if tile.hidden {
+                            rectangle(WHITE, rect, transform, gl);
                         } else {
-                            let mut color:[f32; 4];
-                            match tile.value{
-                                5 => color = [1.0, 0.78, 1.0, 1.0],
-                                4 => color = [0.74, 0.7, 1.0, 1.0],
-                                3 => color = [0.63, 0.77, 1.0, 1.0],
-                                2 => color = [0.61, 0.96, 1.0, 1.0],
-                                1 => color = [0.79, 1.0, 0.75, 1.0],
-                                0 => color = [0.99, 1.0, 0.71, 1.0],
-                                _ => color = [1.0, 0.84, 0.3529, 1.0],
-                            }
-                            //let mut color:[f32; 4] = [0.0, 1.0, 0.1 + 0.35*tile.value as f32, 1.0];
-                            rectangle(color, rect, transform, gl);
-                            if tile.value != 0 {
-                                text::Text::new_color(WHITE, 32/(game.x_size as f32 *0.1) as u32).draw(&tile.value.to_string(), &mut glyph_cache, &DrawState::default(), c.transform.trans(x * tile_size[0] + (tile_size[0]/4.5), (y * tile_size[1]) + (tile_size[1]/1.2)), gl).unwrap();
+                            if tile.value == -1 {
+                                rectangle([1.0, 0.68, 0.68, 1.0], rect, transform, gl);
+                                
+                            } else {
+                                let mut color:[f32; 4];
+                                match tile.value{
+                                    5 => color = [1.0, 0.78, 1.0, 1.0],
+                                    4 => color = [0.74, 0.7, 1.0, 1.0],
+                                    3 => color = [0.63, 0.77, 1.0, 1.0],
+                                    2 => color = [0.61, 0.96, 1.0, 1.0],
+                                    1 => color = [0.79, 1.0, 0.75, 1.0],
+                                    0 => color = [0.99, 1.0, 0.71, 1.0],
+                                    _ => color = [1.0, 0.84, 0.3529, 1.0],
+                                }
+                                //let mut color:[f32; 4] = [0.0, 1.0, 0.1 + 0.35*tile.value as f32, 1.0];
+                                rectangle(color, rect, transform, gl);
+                                if tile.value != 0 {
+                                    text::Text::new_color(WHITE, 32/(game.x_size as f32 *0.1) as u32).draw(&tile.value.to_string(), &mut glyph_cache, &DrawState::default(), c.transform.trans(x * tile_size[0] + (tile_size[0]/4.5), (y * tile_size[1]) + (tile_size[1]/1.2)), gl).unwrap();
+                                }
                             }
                         }
+                        x += 1.0;
                     }
-                    x += 1.0;
+                    y += 1.0;
+                    x = 0.0;
                 }
-                y += 1.0;
-                x = 0.0;
-            }
 
-            //Draw grid
-            for row in 1..row_len as i32 + 1 {
-                line(DARK_GRAY, 1.0, [row as f64 *tile_size[1], 0.0, row as f64 *tile_size[1], game.grid_size[1] as f64], transform, gl);
-            }
-            for col in 1..col_len as i32 + 1 {
-                line(DARK_GRAY, 1.0, [0.0, col as f64 *tile_size[0], game.grid_size[0] as f64, col as f64 *tile_size[0]], transform, gl);
+                //Draw grid
+                for row in 1..row_len as i32 + 1 {
+                    line(DARK_GRAY, 1.0, [row as f64 *tile_size[1], 0.0, row as f64 *tile_size[1], game.grid_size[1] as f64], transform, gl);
+                }
+                for col in 1..col_len as i32 + 1 {
+                    line(DARK_GRAY, 1.0, [0.0, col as f64 *tile_size[0], game.grid_size[0] as f64, col as f64 *tile_size[0]], transform, gl);
+                }
             }
         });
     }
@@ -170,12 +188,12 @@ impl App {
 fn main() {
 
     let mut game_start = false;
-    let mut game = GameInstance::large();
+    let mut game = GameInstance::empty();
 
     //Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
     // Create a Glutin window.
-    let mut window: PistonWindow = WindowSettings::new("Minesweeper", [game.window_size[0], game.window_size[1]])
+    let mut window: Window = WindowSettings::new("Minesweeper", [game.window_size[0], game.window_size[1]])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -194,7 +212,7 @@ fn main() {
         e.mouse_cursor(|pos| cursor=pos);
 
         if let Some(args) = e.render_args() {
-            app.render(&args, &game);
+            app.render(&args, &mut game);
             
         }
 
